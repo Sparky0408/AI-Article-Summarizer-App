47a6f905013c15af8ccb923c4065418709e39c0e
Ruturaj Shinde <ruturajrs20@gmail.com>
2024-02-03 15:55:21 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-content">
-        <p>{ <span>{new Date().getFullYear()}&copy; Ruturaj Shinde. All Rights Reserved.</span>}</p>
+        <p>&copy { <span>{new Date().getFullYear()}; Ruturaj Shinde. All Rights Reserved.</span>}</p>
       </div>
     </footer>
   );
